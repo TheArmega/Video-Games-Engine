170c28349376371ae369d52a1165880c71596ca9
TheArmega <sanmasja@gmail.com>
2025-10-16 22:59:17 +0200

    VIDENG-10 Implemented code that uses SFML module and displays a window with a circle in it

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Video-Games-Engine
-In this repo I will save all the work I will be doing in orde to build a game engine.
+In this repo I will save all the work I will be doing in orde to build a game engine :)
+
+Right now there is only working a basic application that show's a greeb BRAT circle in a window!
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
 
-int main() { std::cout << "Hello World!" << std::endl; }
+int main() {
+
+  int const width = 1440;
+  int const height = 1080;
+  sf::RenderWindow window(sf::VideoMode({width, height}), "VideoGameEngine");
+
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color(138, 206, 0));
+  shape.setOrigin({shape.getRadius(), shape.getRadius()});
+  shape.setPosition({float(width) / 2, float(height) / 2});
+
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>())
+        window.close();
+    }
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+}
